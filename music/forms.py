43c3c19375f112a9c869460bd9c0683b5cda221b
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-22 15:09:42 +0530

    Add new Signup form

---
+++ music/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class SignupForm(UserCreationForm):
+    first_name = forms.CharField(max_length=200, required=False, help_text="First Name")
+    last_name = forms.CharField(max_length=200, required=False, help_text="Last Name")
+    email = forms.CharField(max_length=200, required=False, help_text="Enter email")
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2',)
