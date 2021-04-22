5c64349b6af361e10bc4c1e52cdfb600911e34a1
Vikas Bahuguna <vikasedu10@gmail.com>
2021-04-22 12:40:51 +0530

    Add path to include music.urls

--- musicpey/urls.py
+++ musicpey/urls.py
@@ -13,9 +13,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path("music/", include('music.urls')),
+    path("", RedirectView.as_view(url="music/", permanent=True)),
+
 ]
+urlpatterns += static(settings.STATIC_ROOT, document_type=settings.STATIC_URL)
+
